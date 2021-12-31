7129920a5f8185ef80010af4add8f962055ea19c
Sooraj <ooraj@sooraj.lan>
2021-12-31 18:58:54 +0100

    Logging and loop counts added to sudoku

--- sudoku.sh
+++ sudoku.sh
@@ -8,7 +8,7 @@ scope="sudoku"
 create_log
 create_empty_matrix
 
-
+counter=0
 for ((new_row=1;new_row<10;new_row++))
 do
 create_row $new_row
@@ -16,6 +16,7 @@ wrng_num=( $(find_wrong_num $new_row) )
 
 until [[ ${#wrng_num[@]} == 0 ]]
 do
+	((counter++))
 	swap_wrong_num "${wrng_num[@]}"
 	if [[ $? == 100 ]]
 	then
@@ -30,4 +31,5 @@ do
 done
 done
 
+echo "Total Loops:$counter"
 print_matrix 0
